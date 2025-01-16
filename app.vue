2e804ebe0404786a029e16def71c81fd356da8d2
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2025-01-16 14:32:52 +0300

    landing page

--- app.vue
+++ app.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "My Portfolio", // Set global title for the app
+    };
+  },
+};
+</script>
+
+<style>
+/* Add any global styles here if needed */
+</style>
